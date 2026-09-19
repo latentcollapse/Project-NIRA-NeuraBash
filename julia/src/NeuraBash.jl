217db6faeb729d0eb963f6893ad9dbe8b8c3481c
Matt C <latentcollapse@gmail.com>
2026-09-19 16:56:18 -0400

    MVP persistence: julia/src/NeuraBash.jl

--- julia/src/NeuraBash.jl
+++ julia/src/NeuraBash.jl
@@ -2,8 +2,10 @@ module NeuraBash
 __precompile__(true)
 include("core_ops.jl")
 include("jul_syntax.jl")
+include("mvp_exec.jl")
 using .CoreOps
 using .JULSyntax
-export CoreOps, JULSyntax
+using .MVPExec
+export CoreOps, JULSyntax, MVPExec
 export core_ops_registry, invoke_core
 end
